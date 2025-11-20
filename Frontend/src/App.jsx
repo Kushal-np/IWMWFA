66a940073143d2fdcb747f9e6bf479463f6c02f3
Kushal-np <wrckushal@gmail.com>
2025-11-20 19:55:09 +0545

    initial setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { Nav } from "./component/Nav";
+
+function App(){
+  return(
+    <div>
+      <Nav/>
+    <div className=" flex justify-center items-center bg-yellow-200 border-2 border-solid text-red-800 text-3xl border-black h-screen ">
+    Waste management system
+    <div className="border-2 border-solid border-black bg-black ">
+    dfljfsdlj
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default App ; 
---
+++ Frontend/src/component/Nav.jsx
@@ -0,0 +1,10 @@
+export function Nav() {
+    return(
+        <div className="flex justify-between items-center text-4xl">
+            <div>hello</div>
+            <div>hello</div>
+            <div>hello</div>
+            <div>hello</div>
+        </div>
+    )
+}
