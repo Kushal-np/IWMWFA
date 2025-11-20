9513d8f90c640fdda617c7b2d5ffbd0969a388bf
Kushal-np <wrckushal@gmail.com>
2025-11-20 21:55:15 +0545

    Added the authentication and authorization middleware

---
+++ Backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,46 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+export const isAuthenticated = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Not authorized, token missing",
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    req.user = decoded; 
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid or expired token",
+      error: error.message,
+    });
+  }
+};
+
+export const authorizeRoles = (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      return res.status(401).json({
+        success: false,
+        message: "Not authenticated",
+      });
+    }
+
+    if (!allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({
+        success: false,
+        message: "You are not authorized to access this resource",
+      });
+    }
+    next();
+  };
+};
--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,10 +1,12 @@
 import express from "express" ; 
+import { getCurrentUser, login, logout, register } from "../controllers/user.controller";
 const router = express.Router();
 
 
 router.post("/signup" , register); 
 router.post("/login " , login);
 router.post("/logout" , logout);
+router.get("/getMe" , getCurrentUser);
 
 
 export default router ; 
