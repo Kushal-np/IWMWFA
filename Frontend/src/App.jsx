3c5531f6b03c5ad662f89112e70a7bd5deaf9a69
Kritan <kritanbastola10@gmail.com>
2025-11-21 09:03:51 +0545

    schedule-page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App(){
     <div className=" flex justify-center items-center bg-yellow-200 border-2 border-solid text-red-800 text-3xl border-black h-screen ">
     Waste management system
     <div className="border-2 border-solid border-black bg-black ">
-    dfljfsdlj
+
     </div>
     </div>
     </div>
